a382eb77fcdaa83be3985d13cbc2686a4a3b86f1
luckychip-io <luckychipdev@protonmail.com>
2021-08-08 09:03:49 +0800

    add WBNB

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,11 +3,11 @@ const MastefChef = artifacts.require('MasterChef');
 module.exports = async function(deployer) {
 
   await deployer.deploy(MastefChef,
-    "0x9C10E684EF7CE6069fb6F90A5e9C8A727a13FbaB",
+    "0x30998b6283D428a1e85068F416a1992365d9e0Db",
     "0x5dE4C680d3e306eBbd94b5795905f8234B22D803",
     "0xAe79ddf5FDb9fcdeCfEf3377455f64e6F21eEC69",
     "100000000000000000000",
-    "10753100",
+    "11010000",
     '900000',
     '90000',
     '10000'
