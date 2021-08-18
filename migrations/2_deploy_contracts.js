7d078d5da7a16f642f0e10c2be9c20f463c0dff9
luckychip-io <luckychipdev@protonmail.com>
2021-08-18 11:44:23 +0800

    modify migration script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,13 +3,18 @@ const MastefChef = artifacts.require('MasterChef');
 module.exports = async function(deployer) {
 
   await deployer.deploy(MastefChef,
-    "0x30998b6283D428a1e85068F416a1992365d9e0Db",
+    "0x8909BbBe374cF5158BEB18b1A8D5e93BC3e2C30F",
     "0x5dE4C680d3e306eBbd94b5795905f8234B22D803",
     "0xAe79ddf5FDb9fcdeCfEf3377455f64e6F21eEC69",
+    "0x5dE4C680d3e306eBbd94b5795905f8234B22D803",
+    "0x5dE4C680d3e306eBbd94b5795905f8234B22D803",
+    "0x5dE4C680d3e306eBbd94b5795905f8234B22D803",
     "100000000000000000000",
     "11010000",
     '900000',
-    '90000',
+    '32400',
+    '24300',
+    '24300',
     '10000'
   );
 
--- truffle-config.js
+++ truffle-config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   networks: {
     testnet: {
-      provider: () => new HDWalletProvider(process.env.MNEMONIC, 'https://data-seed-prebsc-1-s1.binance.org:8545'),
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, 'https://data-seed-prebsc-2-s1.binance.org:8545'),
       network_id: 97
     },
     mainnet: {
