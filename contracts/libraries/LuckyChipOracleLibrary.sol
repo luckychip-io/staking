5bacc3d796d93908cf471cac4769af95a54fbaf1
lucky-jack <luckychipdev@protonmail.com>
2021-10-13 18:03:25 +0800

    add MiningOracle

--- contracts/libraries/LuckyChipOracleLibrary.sol
+++ contracts/libraries/LuckyChipOracleLibrary.sol
@@ -6,7 +6,7 @@ import '../interfaces/ILuckyChipPair.sol';
 import './FixedPoint.sol';
 
 // library with helper methods for oracles that are concerned with computing average prices
-library LuckyChipV2OracleLibrary {
+library LuckyChipOracleLibrary {
     using FixedPoint for *;
 
     // helper function that returns the current block timestamp within the range of uint32, i.e. [0, 2**32 - 1]
